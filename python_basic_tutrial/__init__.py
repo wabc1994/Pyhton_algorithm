3a64083a7ba13c4d93017d0651617c6302f2e6c2
wabc1994 <liuxiongcheng145@gmail.com>
2018-05-13 14:49:33 +0800

    the python package for basic python knowledge

---
+++ python_basic_tutrial/__init__.py
@@ -0,0 +1,22 @@
+!  #/User/coderlau/PycharmProject
+# -*- coding: utf-8 -*-
+@author
+
+: liuxiongcheng
+
+
+@contact_me_email
+
+: liuxiongcheng145 @ gmail.com
+
+
+@file
+
+: __init__.py.py
+
+
+@time
+
+: 13 / 05 / 2018
+2: 47
+PM
