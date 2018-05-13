478e6abffcaa7137ac0e991ad636f0ef7048eda9
wabc1994 <liuxiongcheng145@gmail.com>
2018-05-13 16:20:30 +0800

    every element in a array specialize the number in the array using collections.defaultdict

---
+++ data_structure/arrays/delete_nth.py
@@ -0,0 +1,26 @@
+"""
+Given a list lst and a number N, create a new list
+that contains each number of the list at most N times without reordering.
+For example if N = 2, and the input is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2],
+drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3,
+which leads to [1,2,3,1,2,3]
+"""
+import collections
+
+# time complexity O(n^2)
+def delete_nth_naive(array, n):
+    ans = []
+    for num in array:
+        if ans.count(num) < n:
+            ans.append(num)
+    return ans
+#time complexity O(n),using hash tables
+def delete_ath(array, n):
+    result = []
+    #using every value of key is 0
+    counts = collections.defaultdict(int)
+
+    for i in array:
+        if counts[i] < n:
+            result.append(i)
+    return result
