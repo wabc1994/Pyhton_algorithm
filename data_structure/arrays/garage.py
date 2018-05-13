91f96308aa16a460f2c7218540f03d5b7d8c4e80
wabc1994 <liuxiongcheng145@gmail.com>
2018-05-13 18:08:08 +0800

    comment

---
+++ data_structure/arrays/garage.py
@@ -0,0 +1,18 @@
+def garage(initial, final):
+    initial = initial[::]  # create a copy to prevent changes in original "initial"
+    steps = 0
+    seq = []
+    while initial != final:
+        zero = initial.index(0)
+        if zero != final.index(0):
+            car_to_move = final[zero]
+            pos = initial.index(car_to_move)
+            initial[zero], initial[pos] = initial[pos], initial[zero]
+        else:
+            for i in range(len(initial)):
+                if initial[i] != final[i]:
+                    initial[zero], initial[i] = initial[i], initial[zero]
+                    break
+        seq.append(initial[::])
+        steps += 1
+    return steps, seq
