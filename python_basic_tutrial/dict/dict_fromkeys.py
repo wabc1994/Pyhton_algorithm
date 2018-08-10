42755c658ddc26da8dd517c499e0cea26994a5ee
wabc1994 <liuxiongcheng145@gmail.com>
2018-08-10 21:59:57 +0800

    字典的基本用法

---
+++ python_basic_tutrial/dict/dict_fromkeys.py
@@ -0,0 +1,7 @@
+# vowels keys
+"关键在于如何构造一个字典， "
+keys = {'a', 'e', 'i', 'o', 'u' }
+value = 'vowel'
+
+vowels = dict.fromkeys(keys, value)
+print(vowels)
---
+++ python_basic_tutrial/dict/dict_pop.py
@@ -0,0 +1,6 @@
+person = {'name': 'Phill', 'age': 22, 'salary': 3500.0}
+
+result = person.popitem()
+print('person = ',person)
+print('Return Value = ',result)
+"popitem()是不带参数的 和基本的 pop()要带参数 key "
