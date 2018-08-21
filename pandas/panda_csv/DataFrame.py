0ba8d92d77b678818cfc2bcbaa269547bf314695
wabc1994 <liuxiongcheng145@gmail.com>
2018-08-21 12:35:17 +0800

    如何使用字典创造csv文件

---
+++ pandas/panda_csv/DataFrame.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+raw_data = {'first_name': ['Jason', 'Molly', 'Tina', 'Jake', 'Amy'],
+        'last_name': ['Miller', 'Jacobson', ".", 'Milner', 'Cooze'],
+        'age': [42, 52, 36, 24, 73],
+        'preTestScore': [4, 24, 31, ".", "."],
+        'postTestScore': ["25,000", "94,000", 57, 62, 70]}
+df = pd.DataFrame(raw_data,columns = ['first_name', 'last_name', 'age', 'preTestScore', 'postTestScore'])
+print(df)
+df.to_csv("CreateFrame.csv")
